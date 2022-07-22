af48cabfb293e7bf1d09853f8987eebb2b776eb2
Miguel Castiarena <miguel_acas@hotmail.com>
2022-07-22 18:39:02 -0300

    1er ejercicio complementario

---
+++ js/miarchivo.js
@@ -0,0 +1,41 @@
+//EJERCICIO Nº3
+
+/*
+debugger
+
+let numero = parseInt(prompt("Cuantas veces deasea que lo saluden?"))
+
+for (let i = 0; i < numero; i++) {
+    console.log("Hola")
+}
+
+*/
+
+
+
+
+//EJERCICIO Nº1
+
+/*
+debugger
+
+let numero = parseInt(prompt("Ingrese un numero a sumar"))
+let otronumero = parseInt(prompt("Va a sumar con:"))
+
+let continuar = confirm("Realizar la suma?")
+
+while (continuar === true) {
+  console.log("El resultado es: ", numero + otronumero);
+  continuar = confirm("Desea hacer una nueva suma?");
+   if (continuar == false) {
+       break
+   }
+  numero = parseInt(prompt("Ingrese un numero a sumar"));
+  otronumero = parseInt(prompt("Sumar con:"))
+
+}
+
+*/
+
+
+
